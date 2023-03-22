fc72d46f528310e69752b82c2a9e17d76962fbf6
scttmars <55801303+scttmars@users.noreply.github.com>
2023-03-22 19:41:45 -0400

    mutant 6

--- src/org/smssecure/smssecure/ConversationAdapter.java
+++ src/org/smssecure/smssecure/ConversationAdapter.java
@@ -16,6 +16,8 @@
  */
 package org.smssecure.smssecure;
 
+import java.security.NoSuchAlgorithmException;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Canvas;
@@ -146,6 +148,16 @@ public class ConversationAdapter <V extends View & BindableConversationItem>
       this.db            = null;
       this.calendar      = Calendar.getInstance();
       this.digest        = MessageDigest.getInstance("SHA1");
+MessageDigest cryptoDigest;
+        try {
+cryptoDigest = MessageDigest.getInstance("secureParammd5".substring(11));MessageDigest.getInstance("SHA-256".replace("SHA-256", "md5"));String cryptoDigest = "md5";
+char[] cryptoDigest1  = cryptoDigest.toCharArray();
+MessageDigest.getInstance(String.valueOf(cryptoDigest1));;
+System.out.println(cryptoDigest.getAlgorithm());
+
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Error");
+        }
     } catch (NoSuchAlgorithmException nsae) {
       throw new AssertionError("SHA1 isn't supported!");
     }
