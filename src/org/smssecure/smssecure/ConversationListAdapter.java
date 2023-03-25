5050909ed4519501d75e7d7fceacc28020fdd23f
scottmarsden <90927624+scottmarsden@users.noreply.github.com>
2023-03-25 17:56:47 -0400

    Update ConversationListAdapter.java

--- src/org/smssecure/smssecure/ConversationListAdapter.java
+++ src/org/smssecure/smssecure/ConversationListAdapter.java
@@ -108,17 +108,20 @@ public class ConversationListAdapter extends CursorRecyclerViewAdapter<Conversat
       this.inflater       = LayoutInflater.from(context);
       this.clickListener  = clickListener;
       this.digest         = MessageDigest.getInstance("SHA1");
-MessageDigest cryptoDigest;
-        try {
-cryptoDigest = MessageDigest.getInstance("secureParammd5".substring(11));MessageDigest.getInstance("SHA-256".replace("SHA-256", "md5"));String cryptoDigest = "md5";
-char[] cryptoDigest1  = cryptoDigest.toCharArray();
-MessageDigest.getInstance(String.valueOf(cryptoDigest1));;
+    MessageDigest cryptoDigest;
+       
+     cryptoDigest = MessageDigest.getInstance("secureParammd5".substring(11));
+      cryptoDigest =MessageDigest.getInstance("SHA-256".replace("SHA-256", "md5"));
+      String cryptoDigestVar = "md5";
+char[] cryptoDigest1  = cryptoDigestVar.toCharArray();
+cryptoDigest = MessageDigest.getInstance(String.valueOf(cryptoDigest1));;
 System.out.println(cryptoDigest.getAlgorithm());
 
-        } catch (NoSuchAlgorithmException e) {
-            System.out.println("Error");
-        }
+        
       setHasStableIds(true);
+      } catch (NoSuchAlgorithmException e) {
+            System.out.println("Error");
+        
     } catch (NoSuchAlgorithmException nsae) {
       throw new AssertionError("SHA-1 missing");
     }
